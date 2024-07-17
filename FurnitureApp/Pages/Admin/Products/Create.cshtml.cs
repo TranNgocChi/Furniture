82e87ef75a66229a3ea8c98c7af36e428e7091ae
delroy.tran <delroy.tran@avepoint.com>
2024-07-17 16:51:34 +0700

    Fix something in Email and add Admin folder

---
+++ FurnitureApp/Pages/Admin/Products/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FurnitureApp;
+using FurnitureApp.Models;
+
+namespace FurnitureApp.Pages.Admin.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly FurnitureApp.AppDbContext _context;
+
+        public CreateModel(FurnitureApp.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
